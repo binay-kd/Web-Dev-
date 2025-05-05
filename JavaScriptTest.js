5c4a59849551b6d88a7824cc4bb8293069ccf5e9
binay-kd <2021wa86412@wilp.bits-pilani.ac.in>
2025-05-05 13:53:32 +0530

    Create JavaScriptTest.js

---
+++ JavaScriptTest.js
@@ -0,0 +1,7 @@
+git checkout JavaScript
+echo "// JavaScript Test Script - Version 1.0" > JavaScriptTest.js
+git add JavaScriptTest.js
+git commit -m "Add JavaScript test script version 1.0"
+
+echo "// JavaScript Test Script - Version 2.0" >> JavaScriptTest.js
+git commit -am "Update JavaScript test script to version 2.0"
